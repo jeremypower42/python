a30ca89430e13f6e8f98699b2d25dc8726cd63d6
jeremypower42 <55464479+jeremypower42@users.noreply.github.com>
2022-07-07 15:45:26 -0600

    Create guessing_game.py

---
+++ guessing_game.py
@@ -0,0 +1,24 @@
+import random
+import time
+
+print("Welcome, I am going to pick a number between 1 and 100")
+time.sleep(3)
+print("I am now picking a number...")
+time.sleep(3)
+guess = int(input("What is your guess? "))
+
+correct_number = random.randint(1,100)
+
+guess_count = 1
+
+while guess != correct_number:
+  guess_count += 1
+
+  if guess < correct_number:
+    guess = int(input("Wrong, You need to guess higher. What is your guess? "))
+  else:
+    guess = int(input("Wrong, You need to guess lower. What is your guess? "))
+
+
+
+print(f"You got the right answer! the right answer was {correct_number}. It took {guess_count} tries.")
